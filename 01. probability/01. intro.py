65e48cf2a309e63487d2b449f86abdca197f6aaa
AmarKanth <amarkanth.bollu@gmail.com>
2024-02-27 19:06:09 +0530

    changes in intro

--- 01. probability/01. intro.py
+++ 01. probability/01. intro.py
@@ -1,6 +1,14 @@
 """
- A 		=> Event
- P(A) 	=> Probability
+A 		=> Event
+P(A) 	=> Probability
 
- P(A) = preferred-outcomes/all-outcomes
+P(A) = preferred-outcomes/all-outcomes
+"""
+
+"""
+Experimental Probabilities
+P(A) = successful-trails/all-trails
+
+Expected outcome = Theoritical outcome * number of trails
+E(A) = P(A) * n
 """
