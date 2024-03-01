033b0b47443e0cf40a75f466326956b7d9eab1b4
AmarKanth <amarkanth.bollu@gmail.com>
2024-03-01 12:09:06 +0530

    added random generator

--- 01. probability/01. intro.py
+++ 01. probability/01. intro.py
@@ -12,3 +12,29 @@ P(A) = successful-trails/all-trails
 Expected outcome = Theoritical outcome * number of trails
 E(A) = P(A) * n
 """
+
+"""
+Given two numbers A and B and we generate x and y using the random number generator with 
+uniform probability density function [0, A] and [0, B] respectively, 
+what's the probability that x + y is less than C?
+
+Input
+3
+1 1 1
+1 1 2
+1 1 3
+"""
+def calculate_probability(a, b, c):
+    if c <= a + b:
+        if c <= min(a,b):
+            return c*c/(2*a*b)
+        else:
+            return (2*c - min(a,b)) * min(a,b)/(2*a*b)
+    return 1
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        a, b, c = map(int, input().split())
+        res = calculate_probability(a, b, c)
+        print(res)
