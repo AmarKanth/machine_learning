092780a29b7c5ae7732a855eacb473ffebc18eed
AmarKanth <amarkanth.bollu@gmail.com>
2024-04-24 15:47:38 +0530

    Update 01. intro.py

--- 02. probability/01. intro.py
+++ 02. probability/01. intro.py
@@ -24,17 +24,27 @@ Input
 1 1 2
 1 1 3
 """
-def calculate_probability(a, b, c):
-    if c <= a + b:
-        if c <= min(a,b):
-            return (c*c)*1/(2*a*b)
-        else:
-            return c*(c-min(a,b)) * min(a,b)/(2*a*b)
-    return 1
+def calculate_probability(A, B, C):
+    area_rectangle = A * B
+    
+    if C <= 0:
+        return 0
+    elif C >= A + B:
+        return 1
+    else:
+        if C <= A and C <= B:
+            area_triangle = 0.5 * C * C
+        elif C > A and C > B:
+            triangle_out = 0.5 * (C - A) * (C - B)
+            area_triangle = 0.5 * C * C - triangle_out
+        elif C > A:
+            triangle_out = 0.5 * (C - A) * (C - A)
+            area_triangle = 0.5 * C * C - triangle_out
+        elif C > B:
+            triangle_out = 0.5 * (C - B) * (C - B)
+            area_triangle = 0.5 * C * C - triangle_out
+        return area_triangle / area_rectangle
 
-if __name__ == "__main__":
-    n = int(input())
-    for _ in range(n):
-        a, b, c = map(int, input().split())
-        res = calculate_probability(a, b, c)
-        print(res)
+inputs = [(1, 1, 1), (1, 1, 2), (1, 1, 3)]
+results = [calculate_probability(A, B, C) for A, B, C in inputs]
+print(results)
