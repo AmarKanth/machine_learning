d181f0b9fc04546b463f16328d573bde8f14e741
AmarKanth <amarkanth.bollu@gmail.com>
2024-04-24 17:33:52 +0530

    Update 01. intro.py

--- 02. probability/01. intro.py
+++ 02. probability/01. intro.py
@@ -48,3 +48,39 @@ def calculate_probability(A, B, C):
 inputs = [(1, 1, 1), (1, 1, 2), (1, 1, 3)]
 results = [calculate_probability(A, B, C) for A, B, C in inputs]
 print(results)
+
+
+"""
+Isaac has to buy a new HackerPhone for his girlfriend Amy. 
+He is exploring the shops in the town to compare the prices whereupon 
+he finds a shop located on the first floor of a building, 
+that has a unique pricing policy. There are N steps leading to the shop. 
+A numbered ball is placed on each of the steps.
+
+The shopkeeper gives Isaac a fair coin and asks him to toss the coin 
+before climbing each step. If the result of the toss is a 'Heads', 
+Isaac should pick up the ball, else leave it and proceed to the next step.
+
+The shopkeeper then asks Isaac to find the sum of all the numbers he 
+has picked up (let's say S). The price of the HackerPhone is then the 
+expected value of S. Help Isaac find the price of the HackerPhone.
+
+Concept:
+Each ball on a step has a 50% chance of being picked up 
+(when the coin toss results in 'Heads') and a 50% chance of not being picked up 
+(when the coin toss results in 'Tails').
+
+Given n steps, and each step i having a ball with number a(i), the expected value E 
+for each ball being picked is simply 1/2*a(i) because there's a 
+50% chance of picking it up.
+
+Calculation of the Expected Value:
+To find the expected value S of all numbers Isaac picks up:
+E(S)=E(a1)+E(a2)+...+E(an)
+E(ai)=1/2*ai
+"""
+expected_value = 0.0
+numbers_on_the_ball = [1,1,2]
+for number in numbers_on_the_ball:
+    expected_value += 0.5 * number
+print(expected_value)
