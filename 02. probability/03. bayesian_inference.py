01fc2dc584623e8966e856cb5824fcb5a73418af
AmarKanth <amarkanth.bollu@gmail.com>
2024-06-06 16:37:16 +0530

    Update 03. bayesian_inference.py

--- 02. probability/03. bayesian_inference.py
+++ 02. probability/03. bayesian_inference.py
@@ -1,1 +1,13 @@
-
+"""
+1. sets and events
+2. subset
+3. intersections
+4. unions
+5. mutually execlusive events
+6. dependence and independece of sets
+7. conditional probality
+8. law of total probability
+9. aditive rule
+10. multiplication law
+11. Bayes law
+"""
