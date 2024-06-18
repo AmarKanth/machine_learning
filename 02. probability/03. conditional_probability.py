9670dc5b51081487fd2695952a7929503d692572
AmarKanth <amarkanth.bollu@gmail.com>
2024-06-18 14:18:14 +0530

    Update 03. conditional_probability.py

--- 02. probability/03. conditional_probability.py
+++ 02. probability/03. conditional_probability.py
@@ -1,1 +1,14 @@
+"""
+P(A|B) : Means event (A) happening given that another event (B) has already occurred. In probability notation 
+this is denoted as A given B.
+"""
 
+"""
+Conditional Probability Formula when events are dependent
+P(A|B) = P (A ∩ B) / P(B)
+"""
+
+"""
+Conditional Probability Formula when events are independent
+P(A|B) = P (A)
+"""
