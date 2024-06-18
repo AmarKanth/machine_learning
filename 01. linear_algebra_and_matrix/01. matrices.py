80d7f81957c7f1de3826a2a218dd9620a11357cd
AmarKanth <amarkanth.bollu@gmail.com>
2024-06-18 09:25:44 +0530

    Update 01. matrices.py

--- 01. linear_algebra_and_matrix/01. matrices.py
+++ 01. linear_algebra_and_matrix/01. matrices.py
@@ -9,3 +9,14 @@ a = np.array([[1, 2], [4, 5]])
 b = np.array([[2, 3], [6, 7]])
 res = np.add(a, b)
 print(res)
+
+
+"""
+2) Subtract a from b
+"""
+import numpy as np
+
+a = np.array([[2, 3], [6, 7]])
+b = np.array([[1, 2], [4, 5]])
+res = a - b
+print(res)
