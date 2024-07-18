5a3155701531cd5288d26a3871261df11408ef67
AmarKanth <amarkanth.bollu@gmail.com>
2024-07-18 21:01:16 +0530

    changes in matrices product

--- 01. linear_algebra_and_matrix/01. matrices.py
+++ 01. linear_algebra_and_matrix/01. matrices.py
@@ -20,3 +20,16 @@ a = np.array([[2, 3], [6, 7]])
 b = np.array([[1, 2], [4, 5]])
 res = a - b
 print(res)
+
+
+"""
+3) Mlutiple a and b
+[a 	b] [i  j] [a*i+b*k 	a*j+b*i]
+[c 	d] [k  l] [c*i+d*k  c*j+d*i]
+"""
+import numpy as np
+
+a = np.array([[1, 2], [4, 5]])
+b = np.array([[2, 3], [6, 7]])
+res = np.dot(a, b)
+print(res)
