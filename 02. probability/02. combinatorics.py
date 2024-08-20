643ebfc248623edc1971c3398a225479f34bf5cd
AmarKanth <amarkanth.bollu@gmail.com>
2024-08-20 11:59:09 +0530

    Update 02. combinatorics.py

--- 02. probability/02. combinatorics.py
+++ 02. probability/02. combinatorics.py
@@ -1,35 +1,21 @@
 """
-Welcome to the exciting class of Professor Manasa. In each lecture she used to play 
-some game while teaching a new concept. Today's topic is Set Theory. For today's game, 
-she had given a set A = {a1, a2, ...aN} of N integers to her students and asked them 
-to play the game as follows.
-
-At each step of the game she calls a random student and asks him/her to select a 
-non-empty subset from set A such that this subset had not been selected earlier 
-and the sum of subset should be even. This game ends when all possible subsets had 
-been selected. Manasa needs your help in counting the total number of times students 
-can be called assuming each student gives the right answer. While it is given that 
-if two numbers are same in the given set, they have different colors. 
-It means that if a1 = a2, then choosing a1 and choosing a2 will be considered as different 
-sets.
+Count the even sum of subsets in the given array
 """
-from itertools import combinations
-
-def count_even_sum_subsets(elements):
+def count_even_sum_subsets(arr):
     MOD = 10**9 + 7
-    even_sum_count = 0
+    n = len(arr)
+    f = False
 
-    for r in range(1, len(elements) + 1):
-        for combo in combinations(elements, r):
-            if sum(combo) % 2 == 0:
-                even_sum_count += 1
+    for x in arr:
+        if x % 2 == 1:
+            f = True
+            break
 
-    return even_sum_count % MOD
+    exp = n-1 if f else n
+    return pow(2, exp, MOD)-1
 
-N = 4
-A = [2, 4, 6, 1]
-result = count_even_sum_subsets(A)
-print(result)
+res = count_even_sum_subsets([2,4,6,1])
+print(res)
 
 
 """
