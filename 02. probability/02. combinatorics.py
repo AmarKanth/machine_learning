b8390b3c46c639be9819b0691fb1988a2cc7107a
AmarKanth <amarkanth.bollu@gmail.com>
2024-08-26 12:21:07 +0530

    Update 02. combinatorics.py

--- 02. probability/02. combinatorics.py
+++ 02. probability/02. combinatorics.py
@@ -26,9 +26,12 @@ at least ci cards have been picked up before it. (As an example, if a card has a
 value of 3 on it, you can't pick that card up unless you've already picked up 
 3 cards previously) In how many ways can all the cards be picked up?
 """
-def ways_to_pick_cards(n, cards):
+def ways_to_pick_cards(cards):
+    MOD = 1000000007
+    n = len(cards)
     sorted_cards = sorted(cards)
     res = 1
+
     if sorted_cards[0] != 0:
         return 0
     
@@ -37,9 +40,9 @@ def ways_to_pick_cards(n, cards):
         if cnt <= 0:
             res = 0
             break
-        res = (res * cnt)
+        res = (res * cnt) % MOD
     return res
 
 c = [0,0,2]
-res = ways_to_pick_cards(len(c), c)
+res = ways_to_pick_cards(c)
 print(res)
