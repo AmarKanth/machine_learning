3e93176458b9a65463a4cebae82e7cfb7c731064
AmarKanth <amarkanth.bollu@gmail.com>
2024-09-23 12:02:30 +0530

    Update 05. eigen_values_and_vectors.py

--- 01. linear_algebra_and_matrix/05. eigen_values_and_vectors.py
+++ 01. linear_algebra_and_matrix/05. eigen_values_and_vectors.py
@@ -1,1 +1,21 @@
+"""
+Eigenvectors are special vectors that, when a matrix (or transformation) is applied to them, don’t change their 
+direction. They might stretch, shrink, or stay the same size, but their direction remains unchanged.
 
+Eigenvalues are the scaling factors that tell you how much the eigenvector is stretched or shrunk during 
+the transformation.
+
+A = [[2, 0]
+     [0, 1]]
+
+consider two vectors:
+Eigenvector v1=[[1]
+                [0]] 
+v1​ (pointing in the x-direction)
+v1​ is an eigenvector with an eigenvalue of 2.
+
+Eigenvector v2=[[0]
+                [1]] 
+v2 (pointing in the yy-direction)
+v2​ is an eigenvector with an eigenvalue of 1.
+"""
