e575e9c06d4da94af77262c94d659355c84283cb
AmarKanth <amarkanth.bollu@gmail.com>
2024-10-04 14:22:01 +0530

    Update 04. bayes_theorem.py

--- 02. probability/04. bayes_theorem.py
+++ 02. probability/04. bayes_theorem.py
@@ -1,4 +1,16 @@
 """
-Bayes Theorem : It is enables us to calculate the probability of a hypothesis being true given observed evidence.
+Bayes Theorem : 
+It is enables us to calculate the probability of a hypothesis being true given observed evidence.
 P(A∣B) = (P(B∣A) × P(A))​ / P(B)
+
+Mathematical Formulation :
+To derive Bayes’s Theorem, we start with the definition of conditional probability:
+P(A∣B)=P(A∩B)/P(B)
+P(B∣A)=P(A∩B)/P(A)
+
+Rearranging the second equation to express P(A∩B):
+P(A∩B)=P(B∣A)*P(A)
+
+Substitute this into the first equation:
+P(A∣B)=P(B∣A)*P(A)/P(B)​
 """
