bdbd5afaea299cf56b5baf281267282769b0bc74
AmarKanth <amarkanth.bollu@gmail.com>
2024-10-16 20:44:49 +0530

    Update 01. descriptive_statistics.py

--- 03. statistics/01. descriptive_statistics.py
+++ 03. statistics/01. descriptive_statistics.py
@@ -75,41 +75,28 @@ Levels of measurement
 """
 Histogram
 """
-import matplotlib.pyplot as plt
-import numpy as np
+import math
 
 data_set = [1, 9, 22, 24, 32, 41, 44, 48, 57, 66, 70, 73, 75, 76, 79, 82, 87, 89, 95, 100]
-frequency = [1]*20
 desired_intervals = 5
-max_num = max(data_set)
-min_num = min(data_set)
-interval_width = max_num - min_num / desired_intervals
-intervals = np.linspace(min_num, max_num, num=desired_intervals+1)
-interval_bounds = [(intervals[i], intervals[i+1]) for i in range(len(intervals)-1)]
+max_value = max(data_set)
+min_value = min(data_set)
+
+range_val = (min_value + max_value)/desired_intervals
+intervals = [[] for _ in range(desired_intervals)]
+
+for j in data_set:
+    idx = math.floor((j - min_value) / range_val)
+    intervals[idx].append(j)
 
 interval_frequency = [0] * desired_intervals
 
-for data_point in data_set:
-    for i, (lower_bound, upper_bound) in enumerate(interval_bounds):
-        if lower_bound <= data_point <= upper_bound:
-            interval_frequency[i] += 1
-            break
+for idx, interval in enumerate(intervals):
+    interval_frequency[idx] = len(interval)
 
-plt.bar(range(len(interval_frequency)), interval_frequency, align='center')
-plt.xlabel('Intervals')
-plt.ylabel('Frequency')
-plt.title('Histogram Table')
-plt.xticks(range(len(interval_frequency)), [f'{int(lower)}-{int(upper)}' for lower, upper in interval_bounds])
-plt.show()
+total_frequency = len(data_set)
+relative_frequency = [freq / total_frequency for freq in interval_frequency]
 
-total_frequency = sum(frequency)
-relative_frequency = [freq/total_frequency for freq in interval_frequency]
-
-fig, ax = plt.subplots()
-ax.bar(range(len(interval_frequency)), relative_frequency, align='center')
-ax.set_xlabel('Intervals')
-ax.set_ylabel('Relative Frequency')
-ax.set_title('Histogram Table with Intervals and Relative Frequency')
-ax.set_xticks(range(len(interval_frequency)))
-ax.set_xticklabels([f'{int(lower)}-{int(upper)}' for lower, upper in interval_bounds])
-plt.show()
+print(intervals)
+print(interval_frequency)
+print(relative_frequency)
