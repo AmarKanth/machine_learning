7fe3396e30698cb71a1f0e9d97263fd548ea50d1
AmarKanth <amarkanth.bollu@gmail.com>
2024-10-21 17:11:42 +0530

    Update 02. matplotlib.py

--- 04. matplotlib/02. matplotlib.py
+++ 04. matplotlib/02. matplotlib.py
@@ -61,3 +61,36 @@ ax.set_ylabel("Price (Millions)")
 ax.legend(bbox_to_anchor=(1, -.08), ncol=3, frameon=False)
 ax.set_ylim(0, 1.6)
 plt.show()
+
+"""
+Figure Size
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+housing_raw = pd.read_csv(
+    "./data_science/04. matplotlib/data/housing_data.csv",
+    parse_dates=["period_begin", "period_end"],
+    dtype={"region_name":"category"}
+)
+
+ca_housing = housing_raw.loc[
+    housing_raw["region_name"].str.contains('CA')
+].assign(region_name=housing_raw["region_name"].str[0:-11])
+
+ca_housing_pivot = ca_housing.pivot_table(index="period_begin", columns="region_name", values="median_active_list_price"
+).assign(CA_average=lambda x: x.mean(axis=1))
+
+ca_housing_markets = ca_housing_pivot.loc[:, ["San Francisco", "Los Angeles", "San Diego", "CA_average"]]
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.plot(ca_housing_markets.index, ca_housing_markets["San Francisco"] / 1000000, label="San Francisco", color="green")
+ax.plot(ca_housing_markets.index, ca_housing_markets["Los Angeles"] / 1000000, label="Los Angeles", color="purple")
+
+fig.suptitle("Big Title", x=.35)
+ax.set_title("Housing Prices in CA Markets 2017-2022", fontsize=12)
+ax.set_xlabel("Year")
+ax.set_ylabel("Price (Millions)")
+
+ax.legend()
+plt.show()
