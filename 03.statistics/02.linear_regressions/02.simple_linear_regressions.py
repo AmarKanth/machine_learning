4cd5bb63d4f82f5a3bd4e51f5dad4f4e5b12d77d
AmarKanth <amarkanth.bollu@gmail.com>
2025-06-23 23:09:51 +0530

    changes in simple linear regression

--- 03.statistics/02.linear_regressions/02.simple_linear_regressions.py
+++ 03.statistics/02.linear_regressions/02.simple_linear_regressions.py
@@ -1,19 +1,26 @@
 """
+ŷ  : y hat
+β₀ : beta zero
+β₁ : beta one
+ε  : epsilon
+"""
+
+"""
 Simple linear regression
 
 y = β₀ + β₁X₁ + ε
 
-y : y is the variable we are trying to predict and is called dependent variable.
-β₀(beta zero) : intercept(bias)
-β₁(beta one) : coeffiecient(slope)
-X₁ : input feature, it is independent variable
-ε(epsilon) = error term
+y  : It is the variable we are trying to predict and is called dependent variable.
+β₀ : Intercept(bias)
+β₁ : Coeffiecient(slope)
+X₁ : Input feature, it is independent variable
+ε  : Error term
 
 when we using the regression analysis, we want to predict the value of y provided we have 
 the value of x.
 
-but to have regression y is dependent on x in some causal way. whenever there is a 
-change in x, such change must translate into a change in y.
+There should be causal relation between y and x. whenever there is a change in x, 
+such change must translate into a change in y.
 """
 
 """
@@ -24,13 +31,14 @@ has received.
 education.
 3. There is causal relation between two, the more education you get, the higher income 
 you are likely to receive.
-4. Reverse relation is education depends on income. The higher your income, the more years
-you spend educating yourself. The causal relationship is not correct.
-5. In our model there are coefficients, β₁ is coefficient it is before the independent 
-variable. It quantifies the effect of education on income. If β₁=50, then for each 
-additional year of education, your income would grow by $50
+4. Reverse relation is education depends on income. Meaning the more money you have, the 
+more years you spend in school. The causal relationship is not correct.
+5. In our model, there are coefficients. β₁ is the coefficient in front of the 
+independent variable, and it measures the effect of education on income. 
+If β₁ = 50, it means that for each additional year of education, income increases 
+by $50.
 6. In this example think about β₀ is the minimum wage.
-7. ε is error of estimation, the error is the actual difference between the observed 
+7. ε is error of estimation, the error is the actual difference between the real 
 income and the income regression predicted. on average accross all the observations, 
 the error is zero. If you earn more than what the regression is predected, then 
 someone earns less than what the regression predected everything evens out. 
@@ -39,24 +47,23 @@ someone earns less than what the regression predected everything evens out.
 """
 ŷ = b₀ + b₁x₁
 
-ŷ(y hat) : whenever we have hat it is an estimated or predicted value.
+ŷ  : whenever we have hat it is an estimated or predicted value.
 b₀ : is the estimated of the regression constant β₀
 b₁ : is the estimated of β₁ and x
 x₁ : is the sample data for the independent variable
 """
 
 """
-You have an ice-cream shop. You noticed a relationship between the number of cones 
+Questions :
+1)You have an ice-cream shop. You noticed a relationship between the number of cones 
 you order and the number of ice-creams you sell. Is this a suitable situation for 
 regression analysis?
 
 If you run out of cones, you cant sell anymore ice creams, this is not 
 regression analysis material. The two variables go hand in hand as (ususally) 
 each ice cream requires cone.
-"""
 
-"""
-You are trying to predict the amount of beer consumed in the US, 
+2)You are trying to predict the amount of beer consumed in the US, 
 depending on the state. Is this regression material?
 
 Beer_Consumption = β₀ + β₁*Population + β₂*Avg_Temperature + β₃*Brewery_Count + ε
