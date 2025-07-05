5010eec5ba9bce1a89780cab04f2e6a24ff48b24
AmarKanth <amarkanth.bollu@gmail.com>
2025-07-05 16:39:39 +0530

    changes in hypothesis testing

--- 03.statistics/02.hypothesis_testing/01.intro.py
+++ 03.statistics/02.hypothesis_testing/01.intro.py
@@ -90,9 +90,10 @@ Notes:
 p value is the smallest level of significance at which we can still reject the 
 null hypothesis, given the observed sample statistics
 
-p < α we should reject the null hypothesis
+p < α   we should reject the null hypothesis
+p >= α  we failed to reject the null hypothesis
 
-The p-value for a two-tailed test is
+The p-value for a one sided test is
 1-number from the table
 
 The p-value for a two-tailed test is
