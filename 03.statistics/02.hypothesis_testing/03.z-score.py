92c8a49d11a1bba3e7dda8fc88139c4b3b387f5a
AmarKanth <amarkanth.bollu@gmail.com>
2025-07-18 23:51:48 +0530

    changes in z-score

--- 03.statistics/02.hypothesis_testing/03.z-score.py
+++ 03.statistics/02.hypothesis_testing/03.z-score.py
@@ -1,5 +1,5 @@
 """
-Z-score representing the standardized distance of the sample mean from the 
+Z-score represents the standardized distance of the sample mean from the 
 population mean.
 It follows the normal distribution.
 
@@ -12,4 +12,12 @@ Where:
 
 if x̄=μ=0 then z=0 then we accept the null hypothesis.
 if z falls into rejection region then we reject the null hypothesis.
+"""
+
+"""
+What is z-standardization?
+Z-standardization is a statistical procedure used to make data points from different 
+datasets comparable. 
+In this procedure, each data point is converted into a z-score. 
+A z-score indicates how many standard deviations a data point is from the mean of the dataset.
 """
