df53219daa833b837ef3762d0425121f583855f4
AmarKanth <amarkanth.bollu@gmail.com>
2025-07-19 00:58:58 +0530

    changes in hypothesis testing

---
+++ 03.statistics/02.hypothesis_testing/03.chi_Square_test.py
@@ -0,0 +1,12 @@
+"""
+The Chi-square test is a hypothesis test used to determine whether there is a relationship 
+between two categorical variables.
+
+χ² = Σ((Oᵢ - Eᵢ)² / Eᵢ)
+Oᵢ = Observed frequency for category i
+Eᵢ = Expected frequency for category i
+
+df=(r−1)(c−1)
+p: number of lines
+q: number of columns
+"""
