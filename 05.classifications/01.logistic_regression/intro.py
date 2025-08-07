24958a7e626cc051ffc4bb2bba60ca144627a423
AmarKanth <amarkanth.bollu@gmail.com>
2025-08-07 21:46:49 +0530

    added notebook2.ipynb

--- 05.classifications/01.logistic_regression/intro.py
+++ 05.classifications/01.logistic_regression/intro.py
@@ -2,12 +2,12 @@
 Logistic Regression :
 It predicts the probability of an event occuring (ex spam/not-spam, yes/no, 0/1).
 
-p(X) = e^(β₀ + β₁x₁ + ⋯ + βₖxₖ) / (1 + e^(β₀ + β₁x₁ + ⋯ + βₖxₖ))
+ŷ = b₁·x₁ + b₂·x₂ + ... + bₖ·xₖ + a
+f(z) = 1 / (1 + e^(−z))
+f(z) = 1 / (1 + e^(−(b₁·x₁ + b₂·x₂ + ... + bₖ·xₖ + a)))
 
-- p(X) is the probability that the output is 1 given input X
-- β₀ is the intercept (bias)
-- β₁, ..., βₖ are the coefficients
-- x₁, ..., xₖ are the input features
+P(y = 1) = 1 / (1 + e^(−Z))
+P(y = 0) = 1 − 1 / (1 + e^(−Z))
 """
 
 """
@@ -18,4 +18,41 @@ Logistic Regression Assumptions
 3. Normality and Homoscedasticity
 4. No Autocorrelation
 5. No Multilinearity
+"""
+
+"""
+MLE (Maximum likelihood estimation)
+A function which estimates how likely it is that the model at hand 
+describes the real underlying relationship of the variables.
+The bigger the likelihood function, the higher the probability 
+that our model is correct.
+"""
+
+"""
+LLR (Log likelihood Ratio)
+measures if our model is statistically different from LL-null, 
+a.k.a a useless model
+"""
+
+"""
+odds:
+The odds of an event represent the ratio of the probability that the event occurs to the 
+probability that it does not occur.
+
+odds = P(event) / (1 - P(event))
+
+odds-ratio:
+The odds ratio (OR) compares the odds of an event occurring in one group to the odds of 
+it occurring in another group.
+
+oddsratio = odds(Group A) / odds(Group B)
+
+Interpretation:
+- OR = 1 → No difference in odds between groups.
+- OR > 1 → Event is more likely in Group A.
+- OR < 1 → Event is more likely in Group B.
+"""
+
+"""
+Pseudo R-Squared
 """
