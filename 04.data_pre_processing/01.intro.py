c893690709e655dc938b430250f00e541940639a
AmarKanth <amarkanth.bollu@gmail.com>
2025-08-10 17:17:18 +0530

    changes in data preprocessing

---
+++ 04.data_pre_processing/01.intro.py
@@ -0,0 +1,39 @@
+"""
+Data Preprocessing
+------------------
+1. Import data
+2. Clean the data
+3. Split into training and test sets
+
+Modeling
+---------
+1. Build the model
+2. Train the model
+3. Make Predictions
+
+Evaluation
+----------
+1. Calculate the performance metrics
+2. Make a verdict
+"""
+
+"""
+What is feature scaling?
+
+Normalization :
+x' = (x - x_min) / (x_max - x_min)
+
+Standardization :
+x' = (x - μ) / σ
+
+μ : Mean of the feature
+σ : Standard deviation of the feature
+"""
+
+"""
+SimpleImputer
+ColumnTransformer
+OneHotEncoder
+LabelEncoder
+StandardScaler
+"""
