c3a1a856bdd6156804c975efb9b4946d7e18add6
AmarKanth <amarkanth.bollu@gmail.com>
2025-08-10 21:47:43 +0530

    added intro in svm

---
+++ 06.classifications/02.svm/intro.py
@@ -0,0 +1,16 @@
+"""
+It tries to find the best boundary known as hyperplane that separates different classes in 
+the data. 
+It is useful when you want to do binary classification like spam vs. not spam or cat vs. dog.
+"""
+
+"""
+Hyperplane : 
+The hyperplane is halfway between these boundaries(support vectors).
+Boundaries are closest data points from each class.
+"""
+
+"""
+Margin : 
+The distance between the hyperplane and the support vectors.
+"""
