203636d6c8a15c0f2586bf3bfaf7cf923db80999
AmarKanth <amarkanth.bollu@gmail.com>
2025-09-16 18:52:49 +0530

    changes in grid search

---
+++ 13.model_selection/02.grid_search/intro.py
@@ -0,0 +1,10 @@
+"""
+Grid Search
+
+-Grid searching is a method to find the best possible combination of hyper-parameters at 
+ which the model achieves the highest accuracy. 
+-Before applying Grid Searching on any algorithm, Data is used to divided into training and 
+ validation set, a validation set is used to validate the models. 
+-A model with all possible combinations of hyperparameters is tested on the validation set to 
+ choose the best combination.
+"""
