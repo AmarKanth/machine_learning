099dd4ccf23af3d5a25b6cb3ea84c5cb7dd31da1
AmarKanth <amarkanth.bollu@gmail.com>
2025-09-27 14:41:58 +0530

    changes in knn

--- 06.classifications/02.knn/intro.py
+++ 06.classifications/02.knn/intro.py
@@ -1,29 +1,29 @@
 """
 What is one-shot learning?
-One-shot learning is a concept in machine learning where the model is trained to recognize 
-the patterns in datasets from a single example instead of training on large datasets. 
-This is useful when we haven't large datasets. It is applied to find the similarity and 
-dissimilarities between the two images.
+- One-shot learning is a concept in machine learning where the model is trained to recognize 
+  the patterns in datasets from a single example instead of training on large datasets. 
+- This is useful when we haven't large datasets. 
+- It is applied to find the similarity and dissimilarities between the two images.
 """
 
 """
 Explain some measures of similarity which are generally used in Machine learning?
 
 Cosine Similarity: 
-By considering the two vectors in n - dimension we evaluate the cosine 
-of the angle between the two. The range of this similarity measure varies from [-1, 1] 
-where the value 1 represents that the two vectors are highly similar and -1 represents 
-that the two vectors are completely different from each other.
+- By considering the two vectors in n - dimension we evaluate the cosine of the angle 
+  between the two. 
+- The range of this similarity measure varies from [-1, 1] where the value 1 represents 
+  that the two vectors are highly similar and -1 represents that the two vectors are 
+  completely different from each other.
 
 Euclidean or Manhattan Distance: 
-These two values represent the distances between the two 
-points in an n-dimensional plane. The only difference between the two is in the way the two 
-are calculated.
+- These two values represent the distances between the two points in an n-dimensional plane. 
+- The only difference between the two is in the way the two are calculated.
 
 Jaccard Similarity: 
-It is also known as IoU or Intersection over union it is widely 
-used in the field of object detection to evaluate the overlap between the predicted 
-bounding box and the ground truth bounding box.
+- It is also known as IoU or Intersection over union it is widely 
+  used in the field of object detection to evaluate the overlap between the predicted 
+  bounding box and the ground truth bounding box.
 """
 
 """
@@ -60,21 +60,23 @@ distance from the query point, influencing how each neighbor affects the model's
 
 The weights can be assigned using:
 
-Uniform Weighting: All neighbors have equal weight regardless of their distance.
+Uniform Weighting: 
+- All neighbors have equal weight regardless of their distance.
 
-Distance Weighting: Weights are inversely proportional to the distance, giving closer 
-neighbors more influence.
+Distance Weighting: 
+- Weights are inversely proportional to the distance, giving closer neighbors more influence.
 
-User-defined Weights: Weights are assigned based on domain knowledge or specific 
-data characteristics.
+User-defined Weights: 
+- Weights are assigned based on domain knowledge or specific data characteristics.
 
 Effect on Model's Prediction:
+- Uniform Weighting: Simple but may not perform well with noisy data or varied distances.
 
-Uniform Weighting: Simple but may not perform well with noisy data or varied distances.
+Distance Weighting: 
+- Improves accuracy by emphasizing closer neighbors, useful for irregular 
+  class boundaries but sensitive to anomalies.
 
-Distance Weighting: Improves accuracy by emphasizing closer neighbors, useful for irregular 
-class boundaries but sensitive to anomalies.
-
-User-defined Weights: Optimizes performance when specific insights about the dataset are 
-applied, though less generalizable.
+User-defined Weights: 
+- Optimizes performance when specific insights about the dataset are 
+  applied, though less generalizable.
 """
