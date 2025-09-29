4c2ddda34a0bc0676b74a037806fdf1d768d7ea3
AmarKanth <amarkanth.bollu@gmail.com>
2025-09-29 13:29:32 +0530

    changes in multi linear regerssion

--- 05.regressions/02.multi_linear_regression/intro.py
+++ 05.regressions/02.multi_linear_regression/intro.py
@@ -1,11 +1,11 @@
 """
-What is multiple linear regression?
+Multiple Linear Regression:
 It addresses more complex relationships by including multiple independent variables.
 """
 
 """
 ŷ = β₀ + β₁x₁ + β₂x₂ + ⋯ + βₙxₙ + ε
-
+where:
 - ŷ is the predicted value (y-hat)
 - β₀ is the intercept
 - β₁, β₂, ..., βₙ are the regression coefficients
@@ -14,7 +14,6 @@ It addresses more complex relationships by including multiple independent variab
 
 """
 Adjusted R²:
-
 adj_r² = 1 - [(1 - R²) × (n - 1) / (n - p - 1)]
 where:
 - r² is regular coefficient of determination
@@ -24,28 +23,25 @@ where:
 - It tells you whether adding a variable actually improves the model or not.
 - If a new variable improves the model, adjusted R² goes up
 - If a new variable is useless, adjusted R² goes down
-- Adj_R² ≤ R² (always less than or equal)
-- It helps prevent overfitting by punishing complexity
+- Adj_R² ≤ R²(it is always less than or equal)
+- It helps to prevent overfitting by punishing complexity
 """
 
 """
-Overfitting
-
-- Model performs well during training but fails to predict new observations.
-- Early stopping of the model's training in case of validation training stops increasing but 
-  the training keeps going on.
+Overfitting:
+- Model performs well, during training but fails to predict new observations.
+- Overfitting can be minimize by early stopping of the model's training in case of 
+  validation training stops increasing but the training keeps going on.
 """
 
 """
-Underfitting
-
+Underfitting:
 - It doesn't perform well on the training and on the testing. 
 - It fails to capture the underlying trend of the data.
 """
 
 """
-train and test split
-
+train and test split:
 - The main purpose of keep some data left over is to evalute the model performance 
   after training.
 """
